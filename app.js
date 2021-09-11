5dedd2eaef87b26e4254e89ef8932c4c1e723288
Antony Gaetano <86926752+AntonyGaetano@users.noreply.github.com>
2021-09-10 21:00:56 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,29 +1,26 @@
 var relogio = document.getElementById("cronometro");
-var segundo = 0;
-var minuto = 0;
-var hora = 0;
+var seg = 0;
+var min= 0;
+var hrs= 0;
 
 
 const Iniciar = () =>{
-    var seg;
-    var min;
-    var hrs;
+
+if(seg == 60){
+    seg = 0;
+    minuto++;  
     
-if(segundo == 60){
-    segundo = 0;
-    minuto++;   
+    if(min == 60){
+        min = 0;
+        hrs++;
+    }
 }
 
-else if(minuto == 60){
-    minuto = 0;
-    hora++;
-}
 
-(segundo < 10)? seg = "0"+segundo: seg = segundo;
-(minuto < 10)? min = "0"+minuto: minuto = minuto;
+var format =  (hrs < 10? "0"+ hrs : hrs)+":"+(min < 10? "0"+ min : min) + ":" + (seg < 10? "0"+ seg : seg)
    
 
-relogio.innerHTML = hrs+":"+min+":"+seg;
+relogio.innerHTML = format;
 segundo++;
 }
 
@@ -33,11 +30,11 @@ const Parar = () =>{
 }
 
 const Reset = () =>{
-
+    clearInterval(tmp);
     minuto = 0;
     segundo = 0;
     segundo = 0;
-    clearInterval(tmp);
+   
     document.getElementById("iniciar").innerHTML="Iniciar";
     relogio.innerHTML = "00:00:00";
 
